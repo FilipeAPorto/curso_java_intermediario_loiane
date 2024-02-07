9e28a0df1caa868847c96cab50615c78d0fd5e3c
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-02-07 18:57:52 -0300

    Aula 57 -  Wrappers Descontinuado

---
+++ .Programas/src/com/filipe/cursojava/b_elementos_basicos/aula57_wrappers/TesteWrapperDescontinuado.java
@@ -0,0 +1,36 @@
+package com.filipe.cursojava.b_elementos_basicos.aula57_wrappers;
+
+public class TesteWrapperDescontinuado {
+    public static void main(String[] args) {
+
+        short num1 = 1;
+        byte num2 = 10;
+        int num3 = 100;
+        long num4 = 10000l;
+
+        float num5 = 3.5f;
+        double num6 = 3.5555;
+
+        boolean flag = true;
+        char a = 'a';
+
+
+        //Short num7 = new Short((short) 1); Obsoleto desde o JAVA 9 e foi removido
+        //Byte num8 = new Byte((byte) 10); Obsoleto desde o JAVA 9 e foi removido
+        // Integer num9 = new Integer(100); Obsoleto desde o JAVA 9 e foi removido
+        //Long num10 = new Long(1000l); Obsoleto desde o JAVA 9 e foi removido
+        //Float num11 = new Float(3.5f); Obsoleto desde o JAVA 9 e foi removido
+        //Double num12 = new Double(3.555); Obsoleto desde o JAVA 9 e foi removido
+        //Boolean flag2 = new Boolean(true); Obsoleto desde o JAVA 9 e foi removido
+        //Character b = new Character('b'); Obsoleto desde o JAVA 9 e foi removido
+
+
+
+    }
+
+
+
+
+
+
+}
