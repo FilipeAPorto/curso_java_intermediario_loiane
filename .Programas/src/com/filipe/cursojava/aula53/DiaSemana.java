70232e9a2d19cbabc7035203b5f7094c360a62fd
Filipe Alves Porto <filipealvesmissias@hotmail.com>
2024-02-07 10:37:07 -0300

    Aula 53 - Enumeradores (Enum)

---
+++ .Programas/src/com/filipe/cursojava/aula53/DiaSemana.java
@@ -0,0 +1,9 @@
+package com.filipe.cursojava.aula53;
+
+public enum DiaSemana { // CAIXA ALTA SEMPRE , SEGUE O MSM CONCEITO DE CONSTANTES
+
+    SEGUNDA,TERCA,QUARTA,QUINTA,SEXTA,SABADO,DOMINGO;
+
+
+
+}
